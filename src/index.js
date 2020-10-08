9d72529acc5660573d296c616d72265b0c8d211f
Vaibhav <mailvaibhavchopra@gmail.com>
2020-10-08 19:08:55 +0530

    basic functionality

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import './scss/style.scss';
 import Sortable from 'sortablejs';
 import _ from 'lodash';
 import { uuid } from 'uuidv4';
-// import 'materialize-css/js/buttons';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// import 'materialize-css/dist/js/materialize';
 import { Modal } from 'materialize-css';
+// import 'materialize-css/js/buttons';
+// import 'materialize-css/js/modal';
 
 // document.addEventListener('DOMContentLoaded', function () {
 //   var elems = document.querySelectorAll('.fixed-action-btn');
 //   var instances = M.FloatingActionButton.init(elems, options);
 // });
-
+let addTable = null;
 const populateTables = (tables) => {
   let tablesHTML = '';
+  let modalsHTML = '';
   Object.keys(tables)
     .sort()
     .forEach((key) => {
@@ -20,44 +26,79 @@ const populateTables = (tables) => {
       tablesHTML += `
         <div class="col s12 m6 l3 z-depth-3 kanban-table-container" >
             <div class="row valign-wrapper">
-              <div class="col s10">
-                <h5>${key}</h5>
-              </div>
-              <div class="col s2">
-                <span class="material-icons"> more_vert </span>
-              </div>
+                <div class="col s10">
+                    <h5>${key}</h5>
+                </div>
+                <div class="col s2">
+                    <span class="material-icons"> more_vert </span>
+                </div>
             </div>
             <div class="row tasks-wrapper" id="${key}">
               ${Object.keys(tableData.tasks)
                 .map((key) => {
-                  return `<div class="col white task" id="${key}">
-                <div class="row priority-wrapper">
-                  <span class="white-text red badge left">Low Priority</span>
-                </div>
-                <p class="task-text">${tableData.tasks[key].title}</p>
-                <div class="row">
-                  <div class="col s12">
-                    <span class="material-icons right"> delete </span>
-                  </div>
-                </div>
-                    </div>`;
+                  return `
+                <div class="col white task" id="${key}">
+                    <div class="row priority-wrapper">
+                        <span class="white-text red badge left">Low Priority</span>
+                    </div>
+                    <p class="task-text">${tableData.tasks[key].title}</p>
+                    <div class="row">
+                        <div class="col s12">
+                            <span class="material-icons right"> delete </span>
+                        </div>
+                    </div>
+                </div>`;
                 })
                 .join('')}
             </div>
-            <a
-              class="btn-floating btn-large waves-effect waves-light primary fixed-action-btn right"
-              ><i class="material-icons">add</i></a
-            >
-          </div>
+            <a class="btn-floating btn-large waves-effect waves-light primary fixed-action-btn right modal-trigger" href="#add-task-modal-${key}">
+                <i class="material-icons">add</i>
+            </a>
+        </div>
+        `;
+      modalsHTML += `
+                <form class="add-task-form" id="add-task-${key}">
+                    <div id="add-task-modal-${key}" class="modal">
+                        <div class="modal-content">
+                            <h4>Add a Task to "${key}"</h4>
+                            <div class="row">
+                                <div class="input-field col s12 primary">
+                                    <input
+                                        placeholder="Ex. Buy Groceries"
+                                        id="task-name-input-${key}"
+                                        type="text"
+                                        class="task-name-input"
+                                    />
+                                    <label for="task-name-input-${key}" class="active">New Task Name</label>
+                                </div>
+                            </div>
+                             <div class="row">
+                                <div class="input-field col s12 primary">
+                                    <input
+                                        placeholder="Ex. Don't forget apples for the pie"
+                                        id="task-description-input-${key}"
+                                        class="task-description-input"
+                                        type="text"
+                                    />
+                                    <label for="task-description-input-${key}" class="active">New Task Description</label>
+                                </div>
+                            </div>
+                        </div>
+                        <input class="table-id-text-input" value="${key}">
+                        <div class="modal-footer">
+                            <div class="modal-footer">
+                                <button class="btn waves-effect primary waves-light">Add</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
         `;
     });
   document.querySelector('#kanban-tables-collection').innerHTML = tablesHTML;
+  document.querySelector('#add-table-modals-container').innerHTML = modalsHTML;
 };
 
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-
-const app = firebase.initializeApp({
+firebase.initializeApp({
   apiKey: 'AIzaSyD6TCMmeg1OCL4J1rgyIJk54H2Hu-KHVMo',
   authDomain: 'kanban-work-manager-7ba68.firebaseapp.com',
   databaseURL: 'https://kanban-work-manager-7ba68.firebaseio.com',
@@ -92,24 +133,61 @@ projectDoc.onSnapshot((doc) => {
         //   projectDoc.update({
         //     ["taskTables."+from] :
         // })
+        console.log(from);
+        console.log(to);
+        if (from != to) {
+          await projectDoc.update({
+            ['taskTables.' + to + '.tasks']: {
+              ...data.taskTables[to].tasks,
+              [uuid()]: data.taskTables[from].tasks[taskId],
+            },
 
-        await projectDoc.update({
-          ['taskTables.' + to + '.tasks']: {
-            ...data.taskTables[to].tasks,
-            [uuid()]: data.taskTables[from].tasks[taskId],
-          },
-
-          ['taskTables.' + from + '.tasks']: _.omit(
-            data.taskTables[from].tasks,
-            taskId
-          ),
-        });
+            ['taskTables.' + from + '.tasks']: _.omit(
+              data.taskTables[from].tasks,
+              taskId
+            ),
+          });
+        }
       },
     });
   });
-  //   document.querySelector('#add-table').addEventListener('click', () => {});
-  document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
+
+  const elem = document.querySelector('#add-table-modal');
+  const instance = M.Modal.init(elem, {});
+
+  const addTaskForms = document.querySelectorAll('.add-task-form');
+  addTaskForms.forEach((addTaskForm) => {
+    addTaskForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      console.log('submitted');
+      const taskTableId = addTaskForm.querySelector('.table-id-text-input')
+        .value;
+      await projectDoc.update({
+        ['taskTables.' + taskTableId + '.tasks']: {
+          ...data.taskTables[taskTableId].tasks,
+          [uuid()]: {
+            title: addTaskForm.querySelector('.task-name-input').value,
+            description: addTaskForm.querySelector('.task-description-input')
+              .value,
+          },
+        },
+      });
+    });
   });
+  const addTableForm = document.querySelector('#add-table-form');
+  addTableForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const newTableName = addTableForm['table-name-input'].value;
+    console.log(newTableName);
+
+    await projectDoc.update({
+      taskTables: {
+        ...data.taskTables,
+        [newTableName]: {
+          tasks: {},
+        },
+      },
+    });
+  });
+  M.AutoInit();
 });
